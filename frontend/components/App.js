7f40eed9f8dbf38e22a16459d3f5bf654bbb44d1
BugzeeDev <irvingcsylva@gmail.com>
2024-11-18 15:48:58 -0800

    fleshed out class components for todos, todoList, and app.js // Added toggleCompletion method

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+
+let id = 0
+let getId = () => ++id
+
+const initialTodos = [
+  {id: getId(), name: "Drink Coffee", completed: false },
+  {id: getId(), name: "Learn React", completed: false },
+  {id: getId(), name: "Make Custom Website", completed: false },
+]
+
 
 export default class App extends React.Component {
+  state = {
+    todos: initialTodos 
+  }
+
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if (id == td.id) return { ...td, completed: !td.completed}
+        return td
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+        <Form />
       </div>
     )
   }
