e84017c5af46743c7b601d5a801c152f18d56fd0
BugzeeDev <irvingcsylva@gmail.com>
2024-11-18 16:30:53 -0800

    Added input for new items and submit/change handling logic

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -17,6 +17,13 @@ export default class App extends React.Component {
     todos: initialTodos 
   }
 
+  addTodo = ( name ) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    })
+  }
+
   toggleCompletion = id => {
     this.setState({
       ...this.state,
@@ -31,7 +38,7 @@ export default class App extends React.Component {
     return (
       <div>
         <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
-        <Form />
+        <Form  addTodo={ this.addTodo }/>
       </div>
     )
   }
